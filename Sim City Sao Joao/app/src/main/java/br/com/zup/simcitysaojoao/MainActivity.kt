473f72574cecbaff07973ba10f12a200ddf3f031
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-06-10 14:16:15 -0300

    Declarar binding

--- Sim City Sao Joao/app/src/main/java/br/com/zup/simcitysaojoao/MainActivity.kt
+++ Sim City Sao Joao/app/src/main/java/br/com/zup/simcitysaojoao/MainActivity.kt
@@ -2,10 +2,14 @@ package br.com.zup.simcitysaojoao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.zup.simcitysaojoao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
