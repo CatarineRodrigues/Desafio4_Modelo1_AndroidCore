a0929c28c4c6e3aed03d817eca9c0a2fb17751c7
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-06-12 12:06:31 -0300

    Implementar execução dos botoes no valorTotalFragment

--- Sim City Sao Joao/app/src/main/java/br/com/zup/simcitysaojoao/produtos/fragments/ValorTotalFragment.kt
+++ Sim City Sao Joao/app/src/main/java/br/com/zup/simcitysaojoao/produtos/fragments/ValorTotalFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 import br.com.zup.simcitysaojoao.R
 import br.com.zup.simcitysaojoao.databinding.FragmentValorTotalBinding
 
@@ -19,4 +20,33 @@ class ValorTotalFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        clicarBotaoCadastrarNovoProduto()
+        clicarBotaoVerProdutos()
+    }
+
+    private fun clicarBotaoCadastrarNovoProduto() {
+        binding.btnCadastrarNovoProduto.setOnClickListener {
+            irParaCadastrarProdutos()
+        }
+    }
+
+    private fun irParaCadastrarProdutos() {
+        NavHostFragment.findNavController(this).navigate(
+            R.id.action_valorTotalFragment_to_cadastrarProdutosFragment
+        )
+    }
+
+    private fun clicarBotaoVerProdutos() {
+        binding.btnVerProdutos.setOnClickListener {
+            irParaListaCadastrados()
+        }
+    }
+
+    private fun irParaListaCadastrados() {
+        NavHostFragment.findNavController(this).navigate(
+            R.id.action_valorTotalFragment_to_listaCadastradosFragment
+        )
+    }
 }
