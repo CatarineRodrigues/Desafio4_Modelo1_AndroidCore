434cf5fd05ede75f2dea2490c57eb1a56f5eeb40
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-06-12 14:08:52 -0300

    Adicionar constantes

--- Sim City Sao Joao/app/src/main/java/br/com/zup/simcitysaojoao/Constantes.kt
+++ Sim City Sao Joao/app/src/main/java/br/com/zup/simcitysaojoao/Constantes.kt
@@ -1,2 +1,6 @@
 package br.com.zup.simcitysaojoao
 
+const val QUANTIDADE = "Quantidade: "
+const val VALOR_UNIT = "Valor unitário: "
+const val RECEITA = "Receita: "
+const val MSG_VALOR_TOTAL = "O valor total de todos os produtos é de R$ "
